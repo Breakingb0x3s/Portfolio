58992f04f6e3463d127655049fd7fefdd0e7ea3e
breakingB0x3s <rtrobinson28@gmail.com>
2023-07-22 20:11:00 -0500

    Less stupid dots update

--- Counting Dots/countingDots.js
+++ Counting Dots/countingDots.js
@@ -3,6 +3,7 @@ const addDots = document.getElementById('actionAdd');
 const dotContainer = document.getElementById('d1');
 
 let countingLimit = 5;
+let questionType = null;
 //Below is the startup, to set what limit should be set on how many dots should be made
 
 const countStartUp = () =>{
@@ -38,18 +39,24 @@ const addStartUp = () =>{
     //Will require 3 spaces, 2 divs for the 2 sets of dots, and then an input box for the answer.  Need these to generate in the initial div, will use the appendChild but all elements will have to have the class of 'adding'. After that is done will just need to write the math logic
     let dot1 = document.createElement('div');
     let dot2 = document.createElement('div');
+    let addProblem = document.createElement('div');
     let addSolution = document.createElement('div');
+    let addQuestion = document.createElement('div');
     dot1.className += "addDots";
     dot2.className += "addDots";
-    addSolution.className += "addDots";
+    addProblem.className += "addDots";
     dot1.id += "leftDots";
     dot2.id += "rightDots";
+    addProblem.id += "problem";
     addSolution.id += "solution";
+    addQuestion.id += "question";
     addSolution.innerHTML = '<input id="bestGuess" type="number">'; //Will need to use the valueAsNumber property for the input number
     //Above creates the unique 'divs' that are required for the adding dots app, Below is where they will occupy the initial 'div' with the current id of d1
     dotContainer.appendChild(dot1);
     dotContainer.appendChild(dot2);
-    dotContainer.appendChild(addSolution);
+    dotContainer.appendChild(addProblem);
+    addProblem.appendChild(addQuestion);
+    addProblem.appendChild(addSolution);
     //Tested the above code and everything is situated about where I want them, CSS will be a pain but will worry about that later
     action.disabled = true;
     document.getElementById("bestGuess").addEventListener('change', ()=>{
@@ -73,47 +80,131 @@ let generateProblem = () =>{
     let leftSide = document.getElementById("leftDots");
     let rightSide = document.getElementById("rightDots");
     let solution = document.getElementById("bestGuess");
+    let question = document.getElementById("question");
 
     leftSide.innerHTML = "";
     rightSide.innerHTML = "";
     solution.value = null;
+    question.innerHTML = "";
 
-    let newDot = document.createElement('div');
-    let otherDot = document.createElement('div');
-    newDot.setAttribute('class','dot');
-    otherDot.setAttribute('class', 'dot2');
+    let dotOne = document.createElement('div');
+    let dotTwo = document.createElement('div');
+    let dotThree = document.createElement('div');
+    let dotFour = document.createElement('div');
+    dotOne.setAttribute('class','dot');
+    dotTwo.setAttribute('class', 'dot2');
+    dotThree.setAttribute('class', 'dot3');
+    dotFour.setAttribute('class', 'dot4');
 
     let variableOne = Math.floor(Math.random() * countingLimit);
     let variableTwo = Math.floor(Math.random() * countingLimit);
 
     for(a = 0; a < variableOne; a++){
-        leftSide.appendChild(newDot.cloneNode(true));
+        let randomDotColor = Math.floor(Math.random() * 4);
+        switch(randomDotColor){
+            case 0:
+                leftSide.appendChild(dotOne.cloneNode(true));
+                break;
+            case 1:
+                leftSide.appendChild(dotTwo.cloneNode(true));
+                break;
+            case 2:
+                leftSide.appendChild(dotThree.cloneNode(true));
+                break;
+            case 3:
+                leftSide.appendChild(dotFour.cloneNode(true));
+                break;
+            default:
+                console.log(randomDotColor);
+        }
     }
 
     for(b = 0; b < variableTwo; b++){
-        rightSide.appendChild(otherDot.cloneNode(true));
+        let randomDotColor = Math.floor(Math.random() * 4);
+        switch(randomDotColor){
+            case 0:
+                rightSide.appendChild(dotOne.cloneNode(true));
+                break;
+            case 1:
+                rightSide.appendChild(dotTwo.cloneNode(true));
+                break;
+            case 2:
+                rightSide.appendChild(dotThree.cloneNode(true));
+                break;
+            case 3:
+                rightSide.appendChild(dotFour.cloneNode(true));
+                break;
+            default:
+                console.log(randomDotColor);
+        }
+    }
+
+    let randomQuestion = Math.floor(Math.random() * 4);
+
+    switch(randomQuestion){
+        case 0:
+            question.innerHTML = '<p>How many <font color="pink">PINK</font> dots are there in total?</p>';
+            questionType = 0;
+            break;
+        case 1: 
+            question.innerHTML = '<p>How many <font color="blue">BLUE</font> dots are there in total?</p>';
+            questionType = 1;
+            break;
+        case 2:
+            question.innerHTML = '<p>How many <font color="green">GREEN</font> dots are there in total?</p>';
+            questionType = 2;
+            break;
+        case 3:
+            question.innerHTML = '<p>How many <font color="purple">PURPLE</font> dots are there in total?</p>';
+            questionType = 3;
+            break;
+        default:
+            console.log('An error has occured');
     }
 
     console.log('The answer is ' + (variableOne + variableTwo));
     console.log('The left side has ' + leftSide.childElementCount + ' dots');
     console.log('The right side has ' + rightSide.childElementCount + ' dots');
+    console.log('Question set ' + randomQuestion);
+    console.log(document.getElementsByClassName('dot').length + 'pink dots');
+    console.log(document.getElementsByClassName('dot2').length + 'blue dots');
+    console.log(document.getElementsByClassName('dot3').length + 'green dots');
+    console.log(document.getElementsByClassName('dot4').length + 'purple dots');
 
     addDots.addEventListener('click', checkSolution);
     addDots.removeEventListener('click', generateProblem);
-    //Need to make a function for checking the guess.  If the guess is wrong I don't want the above function to run, however if it is correct I want a popup to congradulate the user and then run the above code for a new problem.
+    //Made some adjustments above to randomly decide the color of the dots, this should allow for more complex questions
 }
-
+//Stopping here, need to adjust checkSolution, I need to find a way to count the specific children within the two containers
 let checkSolution = () =>{
     let guess = document.getElementById("bestGuess").value * 1;
+    let questionCheck = "";
 
-    if(guess === (document.getElementById("leftDots").childElementCount + document.getElementById("rightDots").childElementCount)){
+    switch(questionType){
+        case 0:
+            questionCheck = document.getElementsByClassName('dot').length;
+            break;
+        case 1:
+            questionCheck = document.getElementsByClassName('dot2').length;
+            break;
+        case 2:
+            questionCheck = document.getElementsByClassName('dot3').length;
+            break;
+        case 3:
+            questionCheck = document.getElementsByClassName('dot4').length;
+            break;
+        default:
+            questionCheck = 0;
+    }
+
+    if(guess === questionCheck){
         if(window.confirm("You got it! Want to do another one? 😽")){
             generateProblem();
         }else{
             location.reload();
         };
         
-    }else if(guess > (document.getElementById("leftDots").childElementCount + document.getElementById("rightDots").childElementCount)){
+    }else if(guess > questionCheck){
         alert('That guess seems high, please count all of the dots again 🤔');
     }else{
         alert('That guess is a bit too low. How about we try again? 😅');
