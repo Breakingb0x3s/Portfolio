88d46df0db9bbc91057226bbb04c0262170d5e4f
breakingB0x3s <rtrobinson28@gmail.com>
2025-02-05 23:48:12 -0600

    Adding new recipe project

---
+++ Family Recipes/famRecipes.js
@@ -0,0 +1,17 @@
+let recipeList = [];
+const herbChickenZ = {
+    recipeName: 'Garlic Herb Chicken and Zucchini',
+    recipeIngredients: [['chicken', 1, 'lbs'], ['salt', 'taste'], ['black pepper', 'taste'], ['butter', 2, 'tbsp'], ['garlic cloves', 2], ['oregano', 0.5, 'tbsp'], ['thyme', 0.5, 'tbsp'], ['zucchini', 0.5, 'cups'], ['cooking wine', 2, 'tbsp'], ['lemon', 1], ['italian parsley', 0.5, 'tbsp']],
+    recipeTags: ["all","fowl"],
+    baseServingSize: 3,
+    recipeLink: 'https://rasamalaysia.com/garlic-herb-chicken-and-zucchini/#wprm-recipe-container-760335',
+    recipeImage: '<img src="https://breakingb0x3s.github.io/Portfolio/recipeImg/herbchickenzucchini.jpg">'
+}
+const garlicSalmon = {
+    recipeName: 'Garlic Butter Salmon and Aspargus',
+    recipeIngredients: [['salmon', 1.5, 'lbs'], ['salt', 1, "tsp"], ['asparagus', 1, 'lbs'], ['garlic cloves', 3], ['parsley', 1, 'tbsp'], ['onion powder', 1, 'tbsp'], ['black pepper', 0.5, 'tsp'], ['lemon', 1]],
+    recipeTags: ["all","seaFood"],
+    baseServingSize: 4,
+    recipeLink: 'https://thatlowcarblife.com/garlic-butter-salmon-and-asparagus/#wprm-recipe-container-2525',
+    recipeImage: '<img src="https://breakingb0x3s.github.io/Portfolio/recipeImg/garlicbuttersalmon.jpg">'
+}
