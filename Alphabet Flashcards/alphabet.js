1c988682f3076874fa7bee4f76e19e0c317d880f
breakingb0x3s <rtrobinson28@gmail.com>
2025-05-10 18:04:35 -0500

    Updated colors and images as some links broke

--- Alphabet Flashcards/alphabet.js
+++ Alphabet Flashcards/alphabet.js
@@ -1,7 +1,7 @@
 let allLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let allCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let allLow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const colors = ['red', 'blue', 'green', 'purple', 'pink', 'beige', 'yellow', 'orange'];
+const colors = ['red', 'blue', 'green', 'purple', 'pink', 'beige', 'black', 'orange'];
 let letter = document.getElementById('alphabet');
 let goodJob = document.getElementById('good');
 let tryAgain = document.getElementById('try');
